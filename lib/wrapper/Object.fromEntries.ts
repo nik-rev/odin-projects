86fc5db41d66cd8727ddc2126bfd6d9dafaa0e61
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-08 14:23:49 +0100

    feat(lib): add type safe wrapper around Object.fromEntries

---
+++ lib/wrapper/Object.fromEntries.ts
@@ -0,0 +1,9 @@
+const typeSafeObjectFromEntries = <
+  const T extends ReadonlyArray<readonly [PropertyKey, unknown]>,
+>(
+  entries: T,
+): { [K in T[number] as K[0]]: K[1] } => {
+  return Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
+};
+
+export default typeSafeObjectFromEntries;
