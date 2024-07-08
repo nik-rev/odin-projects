57d51e1f9f4ef2280209e24f3c15c7b7a2fa089b
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-08 18:51:34 +0100

    feat(env): add env variables for github

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,13 @@
 import typeSafeObjectFromEntries from "@/lib/wrapper/Object.fromEntries";
 import { z } from "zod";
 
-const envVariables = ["NEXTAUTH_SECRET", "DATABASE_URL", "DIRECT_URL"] as const;
+const envVariables = [
+  "NEXTAUTH_SECRET",
+  "DATABASE_URL",
+  "DIRECT_URL",
+  "AUTH_GITHUB_ID",
+  "AUTH_GITHUB_SECRET",
+] as const;
 
 const envSchema = z.object(
   typeSafeObjectFromEntries(
