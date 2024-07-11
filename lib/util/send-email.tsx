8b5df041923032eef29f96f3bf26f4833534f3f6
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-11 14:38:12 +0100

    feat(lib): add send email utility

---
+++ lib/util/send-email.tsx
@@ -0,0 +1,31 @@
+import VerificationEmail from "@/emails/verify-email";
+import { render } from "@react-email/components";
+import { SOURCE } from "@/constants";
+import { Resend } from "resend";
+
+import env from "../schema/env";
+
+const resend = new Resend(env.EMAIL_SERVER_PASSWORD);
+
+export const sendEmail = async (
+  email: string,
+  subject: string,
+  body: JSX.Element,
+) => {
+  const { error } = await resend.emails.send({
+    from: env.EMAIL_FROM,
+    html: render(body),
+    to: email,
+    subject,
+  });
+
+  if (error) {
+    throw error;
+  }
+};
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const url = `${SOURCE}/auth/new-verification?token=${token}`;
+
+  await sendEmail(email, "Confirm your email", VerificationEmail({ url }));
+};
