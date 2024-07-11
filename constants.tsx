8a5d0a0ed08d452d853c7789be9dc1c864492a79
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-11 14:39:02 +0100

    feat: add time-based constants to constants file

--- constants.tsx
+++ constants.tsx
@@ -1,2 +1,11 @@
-export const APPLICATION_NAME = "NextJS Skeleton";
+// time
 export const CURRENT_YEAR = new Date().getFullYear();
+export const MILLISECOND = 1;
+export const SECOND = 1000 * MILLISECOND;
+export const MINUTE = 60 * SECOND;
+export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;
+
+// metadata
+export const APPLICATION_NAME = "NextJS Skeleton";
+export const SOURCE = "http://localhost:3000";
