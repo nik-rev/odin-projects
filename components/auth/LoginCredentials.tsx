221e20c9edc1ecf1ea45c42531e9e9ffa7cb6180
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-11 14:40:29 +0100

    feat: add <Link> to forgot password page from login form

--- components/auth/LoginCredentials.tsx
+++ components/auth/LoginCredentials.tsx
@@ -1,23 +1,27 @@
 "use client";
 
 import { loginWithCredentials } from "@/actions/auth";
+import Link from "next/link";
 
 import AuthButton from "./AuthButton";
 
 // TODO: validate with zod
 export default function LoginCredentials() {
   return (
-    <form action={loginWithCredentials} className="bg-orange-300">
-      <label htmlFor="email">Email</label>
-      <input placeholder="Email" name="email" type="email" id="email" />
-      <label htmlFor="password">Password</label>
-      <input
-        placeholder="Password"
-        name="password"
-        type="password"
-        id="password"
-      />
-      <AuthButton />
-    </form>
+    <>
+      <form action={loginWithCredentials} className="bg-orange-300">
+        <label htmlFor="email">Email</label>
+        <input placeholder="Email" name="email" type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input
+          placeholder="Password"
+          name="password"
+          type="password"
+          id="password"
+        />
+        <AuthButton />
+      </form>
+      <Link href="/forgot-password">Forgot password?</Link>
+    </>
   );
 }
