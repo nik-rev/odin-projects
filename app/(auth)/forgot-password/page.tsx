64d5ee09d9a935eb8e3594b9d13b4c4b64ee12ef
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-26 18:59:15 +0100

    feat(app/forgot-password): use react-hook-form

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,12 +1,80 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleNotch } from "@phosphor-icons/react";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+
 import { resetPassword } from "@/actions/reset-password";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useIsClient } from "@/hooks/use-is-client";
+import type { TForgotPasswordSchema } from "@/lib/schema/forgot-password";
+import { ForgotPasswordSchema } from "@/lib/schema/forgot-password";
 
 export default function ForgotPassword() {
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const isClient = useIsClient();
+
+  const form = useForm<TForgotPasswordSchema>({
+    resolver: zodResolver(ForgotPasswordSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const handleSubmit = (values: TForgotPasswordSchema) => {
+    startTransition(() => {
+      resetPassword(values).then((data) => {
+        if (data.success) {
+          setSuccess(data.success);
+        }
+        if (data.error) {
+          setError(data.error);
+        }
+      });
+    });
+    form.reset();
+    setSuccess("");
+    setError("");
+  };
+
+  if (!isClient) {
+    return <CircleNotch className="animate-spin" />;
+  }
+
   return (
-    <form action={resetPassword}>
-      <input placeholder="Email" name="email" type="text" />
-      <button type="submit">Send Me Reset link</button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled={isPending} type="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {error}
+        {success}
+        <Button type="submit" disabled={isPending}>
+          Reset
+        </Button>
+      </form>
+    </Form>
   );
 }
