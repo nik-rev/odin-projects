b1ea1a72f7966b57ee520631e78ea1b4371c7e33
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-06 03:23:17 +0100

    feat: add basic search check

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
 const search = document.createElement("input");
 
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
+const isEmptyObject = (object) => Object.keys(object).length === 0;
 
-function onGot(item) {
-  console.log(item)
-  let color = "blue";
-  if (item.color) {
-    color = item.color;
+let instantMacros, searchMacros;
+
+const defaultOptions = {
+  instantMacros: {
+    ".g": "https://www.github.com/",
+    ".y": "https://www.youtube.com/",
+    ".p": "https://www.pass.proton.me",
+    ".m": "https://www.mail.proton.me",
+    ".d": "https://www.drive.proton.me",
+    ".h": "https://news.ycombinator.com/",
+    ".a": "https://claude.ai/new",
+  },
+  searchMacros: {
+    "?y": "https://www.youtube.com/results?search_query=",
+    "?o": "https://www.google.com/search?q=site%3Astackoverflow.com+",
+    "?r": "https://github.com/search?type=repositories&q=",
+    "?c": "https://github.com/search?type=code&q=",
+  },
+};
+
+browser.storage.sync.get().then((obj) => {
+  obj = isEmptyObject(obj) ? defaultOptions : obj;
+  instantMacros = obj.instantMacros;
+  searchMacros = obj.searchMacros;
+  console.log(instantMacros, searchMacros);
+}, console.error);
+
+const checkInstantMacros = (e) => {
+  const query = e.target.value;
+  if (query in instantMacros) {
+    clearInput();
+    open(instantMacros[query], "_self");
   }
-  document.body.style.border = `10px solid ${color}`;
-}
+};
 
-const getting = browser.storage.sync.get("color");
-getting.then(onGot, onError);
+const checkSearchMacros = (e) => {
+  const match = e.target.value.match(/\?(.*?)\s+(.*)/);
+  if (match) {
+    const [_, command, query] = match;
 
+    if (command in searchMacros) {
+      clearInput();
+      open(searchMacros[command].concat(encodeURIComponent(query)), "_self");
+      return true;
+    }
+  }
+  return false;
+};
+
+search.style.display = "none"
 search.style.position = "absolute";
 search.style.zIndex = "10000000000";
 search.style.fontFamily = "monospace";
@@ -46,8 +82,16 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
+search.addEventListener("input", checkInstantMacros);
 search.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     toggleDisplay();
+  } else if (e.key === "Enter") {
+    if (!checkSearchMacros(e)) {
+      open(
+        `https://www.google.com/search?q=${encodeURIComponent(e.target.value)}`,
+        "_self",
+      );
+    }
   }
 });
